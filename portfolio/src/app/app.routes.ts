02c184db11ac4d1df9c036c76bd97431a7288f32
Amanda Mar <msamandamar@gmail.com>
2024-03-14 19:54:02 -0700

    initial homepage layout

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectComponent } from './components/project/project.component';
 
 export const routes: Routes = [
-  { path: '', component: ProjectsComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
   { path: 'projects/categories/:slug', component: ProjectsComponent },
   { path: 'projects/tags/:slug', component: ProjectsComponent },
---
+++ portfolio/src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostListener, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor(private el: ElementRef) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const value = window.scrollY;
+    const bg = this.el.nativeElement.querySelector('#bg');
+    const moon = this.el.nativeElement.querySelector('#moon');
+    const mountain = this.el.nativeElement.querySelector('#mountain');
+    const road = this.el.nativeElement.querySelector('#road');
+    const text = this.el.nativeElement.querySelector('#text');
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+  }
+}
